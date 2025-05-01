35507273e5ff89bdf87c6452b7fff0b3d7472641
Meraj Hosseini <imerajdev@gmail.com>
2025-05-01 03:50:03 +0330

    channel/practice-02 completed

--- channels/practice-02/main.go
+++ channels/practice-02/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func doubleAndSend(n int, ch chan int) {
+	result := n * 2
+	ch <- result
+}
+
+
+func main() {
+	var wg sync.WaitGroup
+	ch := make(chan int, 5)
+
+	for i:=1; i<=5; i++ {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			doubleAndSend(n, ch)
+		}(i)
+	}
+
+	wg.Wait()
+
+	close(ch)
+
+	var sum int
+
+	for result := range ch {
+		sum += result
+	}
+
+	fmt.Println("Total sum: ", sum)
+}
