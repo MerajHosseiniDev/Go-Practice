c59c02e62ccb1737e15a508445d9c502c266535c
Meraj Hosseini <imerajdev@gmail.com>
2025-05-02 03:37:35 +0330

    Select practice-03 UPDATED

--- select/practice-03/main.go
+++ select/practice-03/main.go
@@ -19,4 +19,18 @@ func main() {
 	case <-time.After(5 * time.Second):
 		fmt.Println("No card inserted. session timed out.")
 	}
+
+	pinEntered := make(chan int)
+
+	go func() {
+		time.Sleep(2*time.Second)
+		pinEntered <- 1234
+	}()
+
+	select {
+	case pin := <- pinEntered:
+		fmt.Printf("PIN entered: [%d]\nWelcome!", pin)
+	case <- time.After(3*time.Second):
+		fmt.Println("No PIN entered. Session ended.")
+	}
 }
