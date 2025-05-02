ea573ab6cdac5a1fb1f0bfddcb1aae02ff660151
Meraj Hosseini <imerajdev@gmail.com>
2025-05-02 04:55:08 +0330

    Select/practice-08 Completed!

---
+++ select/practice-08/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task1(ch1 chan string) {
+	time.Sleep(1 * time.Second)
+	ch1 <- "goroutine 1 finished first.."
+}
+
+func task2(ch2 chan string) {
+	time.Sleep(2 * time.Second)
+	ch2 <- "goroutine 2 finished first.."
+}
+
+func task3(ch3 chan string) {
+	time.Sleep(3 * time.Second)
+	ch3 <- "goroutine 3 finished first.."
+}
+
+func main() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	ch3 := make(chan string)
+
+	go task1(ch1)
+	go task2(ch2)
+	go task3(ch3)
+
+	select {
+	case msg1 := <-ch1:
+		fmt.Println("Recieved: ", msg1)
+	case msg2 := <-ch2:
+		fmt.Println("Recieved: ", msg2)
+	case msg3 := <-ch3:
+		fmt.Println("Recieved: ", msg3)
+	}
+}
