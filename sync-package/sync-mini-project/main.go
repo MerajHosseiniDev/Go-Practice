885884b2da791258206240e3c686248a0500eee4
Meraj Hosseini <imerajdev@gmail.com>
2025-05-03 10:36:30 +0330

    mini project Updated

--- sync-package/sync-mini-project/main.go
+++ sync-package/sync-mini-project/main.go
@@ -6,37 +6,41 @@ import (
 	"time"
 )
 
-var (
-	Balance int = 1000
-	mu      sync.Mutex
-)
+var	Balance int = 1000
 
-func withdraw(wg *sync.WaitGroup, amount int) {
+
+func processWithdrawals(wg *sync.WaitGroup, ch <- chan int) {
 	defer wg.Done()
-	mu.Lock()
-	defer mu.Unlock()
 
-	if Balance >= amount {
-		fmt.Printf("Withdrawal %d$ ...", amount)
-		Balance -= amount
-		time.Sleep(1 * time.Second)
-		fmt.Printf("withdrawal of %d$ successful.\nRemaining balance: %d$\n", amount, Balance)
-	} else {
-		fmt.Printf("Insufficient funds for withdrawal of %d$.\nCurrent balance: %d$", amount, Balance)
+	for amount := range ch {
+		if Balance >= amount {
+			fmt.Printf("Withdrawal %d$ ...\n", amount)
+			Balance -= amount
+			time.Sleep(1 * time.Second)
+			fmt.Printf("withdrawal of %d$ successful.\nRemaining balance: %d$\n", amount, Balance)
+		} else {
+			fmt.Printf("Insufficient funds for withdrawal of %d$.\nCurrent balance: %d$\n", amount, Balance)
+		}
+		time.Sleep(500 * time.Millisecond)		
 	}
-	time.Sleep(500 * time.Millisecond)
 }
 
 func main() {
-	var wg sync.WaitGroup
 	amounts := []int{100, 200, 200, 500, 50}
+	withdrawalrequests := make(chan int)
+	var wg sync.WaitGroup
+
 
 	fmt.Printf("Hello and welcome\nBalance: %d$\n", Balance)
 
-	wg.Add(len(amounts))
+	wg.Add(1)
+	go processWithdrawals(&wg, withdrawalrequests)
+
 	for _, amount := range amounts {
-		go withdraw(&wg, amount)
+		withdrawalrequests <- amount
 	}
+
+	close(withdrawalrequests)
 
 	wg.Wait()
 	fmt.Println("Final Amount balance: ", Balance)
