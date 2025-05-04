f56e3ae8b2d473d9144d9eb62e1495918dd55e8d
Meraj Hosseini <imerajdev@gmail.com>
2025-05-04 12:32:51 +0330

    Added perodic count display goroutine

--- mutex/practice-04/main.go
+++ mutex/practice-04/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 type Counter struct {
 	count int
-	mu	  sync.Mutex
+	mu    sync.RWMutex
 }
 
-func (c *Counter) increment(wg *sync.WaitGroup){
+func (c *Counter) increment(wg *sync.WaitGroup) {
 	defer wg.Done()
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -24,21 +25,33 @@ func (c *Counter) decrement(wg *sync.WaitGroup) {
 	c.count--
 }
 
+func (c *Counter) Value() int {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.count
+}
+
 func main() {
 	var wg sync.WaitGroup
 	counter := &Counter{}
 
-	for i:=0; i < 100; i++ {
+	for i := 0; i < 100; i++ {
 		wg.Add(1)
 		go counter.increment(&wg)
 	}
 
-	for i:=0; i<5; i++ {
+	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go counter.decrement(&wg)
 	}
 
-	wg.Wait()
+	go func() {
+		for {
+			fmt.Println("Currrent Count: ", counter.count)
+			time.Sleep(10*time.Millisecond)
+		}
+	}()
+		wg.Wait()
 	fmt.Println("Final Count: ", counter.count)
 
 }
