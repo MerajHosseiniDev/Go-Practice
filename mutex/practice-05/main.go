8a0ecb485d71e6b63ae980baacb7dbf45f7aaec6
Meraj Hosseini <imerajdev@gmail.com>
2025-05-05 07:00:15 +0330

    Updated practice-05: Mutex usage with withdraw

--- mutex/practice-05/main.go
+++ mutex/practice-05/main.go
@@ -14,14 +14,14 @@ type BankAccount struct {
 func (b *BankAccount) deposit(wg *sync.WaitGroup, depositRequest chan int) {
 	defer wg.Done()
 
-	for amount := range depositRequest{
+	for depositAmount := range depositRequest{
 		time.Sleep(500*time.Millisecond)
 
 		b.mu.Lock()
-		b.Balance += amount
+		b.Balance += depositAmount
 		b.mu.Unlock()
 
-		fmt.Printf("Deposit of %d$ successful!\n", amount)
+		fmt.Printf("Deposit of %d$ successful! New balance: %d$\n", depositAmount, b.Balance)
 	}
 }
 
@@ -32,23 +32,47 @@ func (b *BankAccount) CheckBalance() int {
 	return b.Balance
 }
 
+func (b *BankAccount) withdraw(wg *sync.WaitGroup, amount int) bool {
+	defer wg.Done()
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	if b.Balance >= amount {
+		time.Sleep(508*time.Millisecond)
+		b.Balance -= amount
+		fmt.Printf("Withdrawal of %d$ successful! New balance: %d$\n", amount, b.Balance)
+		return true
+	} else {
+		fmt.Printf("Insufficient funds: Tried to withdraw %d$, But only %d$ available\n", amount, b.Balance)	
+		return false
+	}
+	
+}
+
 
 func main() {
 	var wg sync.WaitGroup
 	BankAccount := BankAccount{Balance: 200}
 	depositRequest := make(chan int)
-	amounts := []int{100,1500,50,40}
+	depositAmounts := []int{100,1500,50,40}
+	withdrawAmounts := []int{1000,200,400,100,100}
 
 	wg.Add(1)
 	go BankAccount.deposit(&wg, depositRequest)
 
-	for _, amount := range amounts {
+	for _, amount := range depositAmounts {
 		depositRequest <- amount
 	}
-
+	
 	close(depositRequest)
 	wg.Wait()
 
-	fmt.Printf("Current Balance: %d$\n", BankAccount.CheckBalance())
+	for _, wAmount := range withdrawAmounts {
+		wg.Add(1) 
+		go BankAccount.withdraw(&wg, wAmount)
+	}
+
+	wg.Wait()
+
+	fmt.Printf("Final Current Balance: %d$\n", BankAccount.CheckBalance())
 
 }
